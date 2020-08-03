c2add41a1d1c2d153709650b6822d27c7dd62ead
Patrick O'Melveny <pvomelveny@gmail.com>
2020-08-03 12:44:11 -0700

    Our python is ooold

--- changelogger.py
+++ changelogger.py
@@ -57,7 +57,7 @@ def write_log(payload):
 
 
 def write_initial_log():
-    payload = {TODAY: {"hash": f"{GIT_COMMIT}", "changes": []}}
+    payload = {TODAY: {"hash": "{}".format(GIT_COMMIT), "changes": []}}
     write_log(payload)
 
 
