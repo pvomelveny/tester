ca586f0c4b90ff24bae0178b21f184bf3cef82d9
Patrick O'Melveny <pvomelveny@gmail.com>
2020-08-03 14:37:17 -0700

    path error

--- changelogger.py
+++ changelogger.py
@@ -130,7 +130,7 @@ def cli(keep):
         project_name=GIT_PROJECT, sorted_keys=sorted_keys, changelog=changelog
     )
 
-    out_file = str(CHANGELOG_DIR) + "{}-changelog.html".format(GIT_PROJECT)
+    out_file = str(CHANGELOG_DIR / "{}-changelog.html".format(GIT_PROJECT))
     print(out_file)
     with open(out_file, "w") as f:
         f.write(rendered)
