a608474cf394004bc724ce41b3775a0cb57ff6e4
Patrick O'Melveny <pvomelveny@gmail.com>
2020-08-03 13:30:08 -0700

    commit historyyy

--- changelogger.py
+++ changelogger.py
@@ -7,6 +7,7 @@ from jinja2 import Environment, PackageLoader, select_autoescape
 import os
 from pathlib import Path
 import shutil
+import subprocess
 
 #####
 ## PATH CONSTANTS
@@ -33,7 +34,7 @@ TODAY = date.today().strftime("%Y-%m-%d")
 #####
 ## File Functions
 #####
-def maybe_get_last():
+def maybe_get_data():
     """
     manages the file with commits and dates.
     Returns last commit if possible otherwise None
@@ -57,16 +58,31 @@ def write_log(payload):
 
 
 def write_initial_log():
-    payload = {TODAY: {"hash": "{}".format(GIT_HEAD), "changes": []}}
+    payload = {
+        TODAY: {
+            "hash": "{}".format(GIT_HEAD),
+            "changes": [
+                "This is where we have started logging, stay tuned for real logs"
+            ],
+        }
+    }
     write_log(payload)
 
 
 #####
 ## git Functions
 #####
+def git_history_between(old, new):
+    result = subprocess.run(
+        ["git", "--oneline", "--ancestry-path", "{}..{}".format(old, new)],
+        stdout=subprocess.PIPE,
+    )
+    lines = result.stdout.decode("utf-8").split("\n")
+    return lines
+
 
 #####
-## Template Functions
+## Jinja2 Template Functions
 #####
 
 #####
@@ -74,16 +90,24 @@ def write_initial_log():
 #####
 @click.command()
 def cli():
-    # TODO: remove prints
-    print(GIT_HEAD)
-    print(GIT_URL)
-    print(GIT_PROJECT)
     # Check if this project has previous commits
+    # Load the json if so
     # If not, start the log and end
-    data = maybe_get_last()
+    data = maybe_get_data()
     if not data:
         write_initial_log()
         return
+
+    # Sort the dates in the file, most recent first
+    sorted_keys = sorted(data, reverse=True)
+    last_commit = data[sorted_keys[0]]["hash"]
+    # Bail if there have been no changes, no changes to log
+    if last_commit == GIT_HEAD:
+        return
+
+    # Git new commit history
+    new_commits = git_history_between(last_commit, GIT_HEAD)
+    print(new_commits)
 
 
 if __name__ == "__main__":
