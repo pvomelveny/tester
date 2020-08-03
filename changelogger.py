5fd050c42a53d21a3903a196af58eb4c5f1c58c5
Patrick O'Melveny <pvomelveny@gmail.com>
2020-08-03 12:36:30 -0700

    More functionality to changelogger

--- changelogger.py
+++ changelogger.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import click
+from datetime import date
+import json
 from jinja2 import Environment, PackageLoader, select_autoescape
 import os
 from pathlib import Path
@@ -22,8 +24,49 @@ GIT_HEAD = os.environ["GIT_COMMIT"]
 GIT_URL = os.environ["GIT_URL"]
 GIT_PROJECT = GIT_URL.split("/")[-1].replace(".git", "")
 
+
+#####
+## TODAY
+#####
+TODAY = date.today().strftime("%Y-%m-%d")
+
+#####
+## File Functions
+#####
+def maybe_get_last():
+    """
+    manages the file with commits and dates.
+    Returns last commit if possible otherwise None
+    """
+    data_file = DATA_DIR / (GIT_PROJECT + ".json")
+    if data_file.exists():
+        with open(data_file, "r") as f:
+            data = json.load(f)
+    else:
+        data = None
+
+    return data
+
+
+def write_log(payload):
+    data_file = DATA_DIR / (GIT_PROJECT + ".json")
+    # TODO: remove print
+    print(data_file)
+    with open(data_file, "w") as f:
+        json.dump(payload, f)
+
+
+def write_initial_log():
+    payload = {TODAY: {"hash": f"{GIT_COMMIT}", "changes": []}}
+    write_log(payload)
+
+
 #####
 ## git Functions
+#####
+
+#####
+## Template Functions
 #####
 
 #####
@@ -31,9 +74,16 @@ GIT_PROJECT = GIT_URL.split("/")[-1].replace(".git", "")
 #####
 @click.command()
 def cli():
+    # TODO: remove prints
     print(GIT_HEAD)
     print(GIT_URL)
     print(GIT_PROJECT)
+    # Check if this project has previous commits
+    # If not, start the log and end
+    data = maybe_get_last()
+    if not data:
+        write_initial_log()
+        return
 
 
 if __name__ == "__main__":
